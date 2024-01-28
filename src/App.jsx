0533d2365219a24ee1fe13b8e316a52f2720c07b
Halcyon666 <whalefallcl@gmail.com>
2024-01-28 17:05:03 +0800

    basic tic-tac-toe done

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,15 @@ export function Square({ onSquareClick, value }) {
   return (
     <button
       onClick={onSquareClick}
-      className="bg-[#0f172a] text-[whitesmoke] border-solid border border-black float-left text-[72px] font-bold leading-[168px] h-[168px] w-[168px] mr-[-1px] mt-[-1px] p-0 text-center"
+      className="bg-[#0f172a] text-[whitesmoke] border-solid border border-[whitesmoke] float-left text-[72px] font-bold leading-[168px] h-[168px] w-[168px] -mr-[1px] -mt-[1px] p-0 text-center"
     >
       {value}
     </button>
   );
 }
 
-export default function Board() {
+export function Board({ xIsNext, squares, onPlay }) {
   const [length] = useState(3);
-  // if u need share a state between different childrens components, just define a status in parent component
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
   const caculateWinner = (squares) => {
     const lines = [
@@ -57,9 +54,10 @@ export default function Board() {
     } else {
       nextSquares[index] = "O";
     }
-    setSquares(nextSquares);
     // change the status you need to use setXIsNext, instead of `xIsNext = !xIsNext;`
-    setXIsNext(!xIsNext);
+    // setXIsNext(!xIsNext);
+    // change the player and set history
+    onPlay(nextSquares);
   };
 
   const squaresElement = Array.from({ length: length * length }, (_, index) => (
@@ -77,21 +75,71 @@ export default function Board() {
   let tips;
   const winner = caculateWinner(squares);
   if (winner) {
-    tips = `The winner: is ${winner}`;
+    tips = `The winner: ${winner}`;
   } else {
-    tips = `Next Player: is ${xIsNext ? "X" : "O"}`;
+    tips = `Next Player: ${xIsNext ? "X" : "O"}`;
   }
 
   return (
     <div>
       <div className="flex justify-center">
-        <div className="text-center text-2xl font-bold mt-4 p-2 border border-purple-400 border-solid shadow-lg shadow-cyan-500/50 bg-cyan-500">
-          {" "}
+        <div className="text-center mt-4 p-2 text-2xl text-[whitesmoke] font-bold border border-solid border-purple-400 shadow-lg shadow-cyan-500/50 bg-cyan-500 opacity-85">
           {tips}
         </div>
       </div>
       <div className="flex justify-center my-8 ">
         <div className="grid grid-cols-3">{squaresElement}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  // if u need share a state between different childrens components, just define a status in parent component
+  // const [xIsNext, setXIsNext] = useState(true);
+  // Notice how [Array(9).fill(null)] is an array with a single item, which itself is an array of 9 nulls.
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  // default current move is zero
+  const [currentMove, setCurrentMove] = useState(0);
+  // currentSquare
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  // param passed by Board  ` onPlay(!xIsNext);`
+  const handlePlay = (nextSquares) => {
+    // first currentMove = 0, and next history length is 2, 
+    // the next move is nextHistory.length-1 also can be currentMove+1,
+    // when click, slice (0, 1) to include the nextSquares.
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(history.length);
+    // setXIsNext(!xIsNext);
+  };
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    // setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, index) => {
+    let description;
+    if (index > 0) {
+      description = "Go to move #" + index;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={index}>
+        <button onClick={() => jumpTo(index)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="flex justify-evenly">
+      <Board onPlay={handlePlay} xIsNext={xIsNext} squares={currentSquares} />
+      <div className="mt-24 mr-40 text-[whitesmoke] text-center pt-20 text-2xl h-[500px] w-[500px] border border-solid border-purple-400 shadow-lg shadow-cyan-500/50 bg-cyan-500 opacity-85">
+        <ol>{moves}</ol>
       </div>
     </div>
   );
